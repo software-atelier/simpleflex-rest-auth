1ea0e1921f320d4f7aebefec23f8b15d73c2afc5
Tobias Kamber <tobias@software-atelier.ch>
2022-08-21 18:00:26 +0200

    fix in deployment script

--- deployToNexus.sh
+++ deployToNexus.sh
@@ -1,4 +1,4 @@
 GPG_TTY=$(tty)
 export GPG_TTY
 export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED"
-mvn deploy
+mvn clean install deploy
